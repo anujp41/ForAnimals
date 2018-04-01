6167819fec6cf1b776a754ffd84bff7ba7e4d4ee
Anuj Pant <anuj.pant41@gmail.com>
2018-04-01 16:55:42 -0400

    react-dropzone installed and box show preview on upload

--- src/components/Furbaby.js
+++ src/components/Furbaby.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { createFurbabyThunk } from '../store';
 import './Input.css';
+import Dropzone from 'react-dropzone';
+import request from 'superagent';
 
 class Furbaby extends Component {
 
@@ -9,10 +11,12 @@ class Furbaby extends Component {
     super();
     this.state = {
       name: '',
-      breed: ''
+      breed: '',
+      photo: null
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.onImageDrop = this.onImageDrop.bind(this);
   }
 
   handleSubmit(event) {
@@ -26,8 +30,14 @@ class Furbaby extends Component {
     this.setState({ [name] : value });
   }
 
+  onImageDrop(files) {
+    const photo = files[0];
+    this.setState({ photo });
+  }
+
   render() {
     const { name, breed } = this.state;
+    console.log('photo is ', this.state.photo && this.state.photo.preview);
     return (
       <div className='container'>
         <form onSubmit={this.handleSubmit}>
@@ -41,6 +51,17 @@ class Furbaby extends Component {
               <label>Breed:</label>
               <input type="text" name="breed" value={breed} onChange={this.handleChange}/>
             </li>
+
+            <li style={{textAlign: 'center'}}>
+              <Dropzone
+                multiple={false}
+                accept="image/*" 
+                onDrop={this.onImageDrop.bind(this)}>
+                <p>Drop an image or click to select a file to upload.</p>
+                <img src={this.state.photo && this.state.photo.preview}/>
+              </Dropzone>
+            </li>
+
             <li>
               <input type="submit" value="submit" />
             </li>
