154c6cb210197445f9687b57ac9b552ef7e76aa1
Anuj Pant <anuj.pant41@gmail.com>
2018-04-06 14:32:52 -0400

    added jquery to react

--- src/components/FurbabiesList.js
+++ src/components/FurbabiesList.js
@@ -1,22 +1,30 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import './FurbabiesList.css';
+import $ from 'jquery';
 
 class FurbabiesList extends Component {
 
+  componentDidMount() {
+    $('.furbabyCard').hover(function(event) {
+      console.log('here ', $(event.currentTarget).find('.furbabyNotes'));
+      $(event.currentTarget).find('.furbabyNotes').toggle();
+    });
+  }
+
   render() {
     const { furbabies } = this.props;
-    const furbaby = furbabies[0];
     return (
       <div className='mainContainer'>
       {furbabies.map(furbaby => (
-        <div className='furbabyCard'>
+        <div key={furbaby.id} className='furbabyCard'>
           <div className='wrapper'>
             <img alt="" src={furbaby.photoUrl} />
             <div className='furbaby'>
               <div className='furbabyDetails'>
                 <div className='furbabyName'><b>Name: </b>{furbaby.name}</div>
                 <div className='furbabyBreed'><b>Breed: </b>{furbaby.breed}</div>
+                <div hidden className='furbabyNotes'><b>Notes: </b>Cat is super healthy!</div>
               </div>
             </div>
           </div>
