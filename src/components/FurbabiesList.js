9fbc5e689fd88c9a53b280e015af5128f5d42ebf
Anuj Pant <anuj.pant41@gmail.com>
2018-04-07 23:22:44 -0400

    update furbabies list to show transform

--- src/components/FurbabiesList.js
+++ src/components/FurbabiesList.js
@@ -5,31 +5,37 @@ import $ from 'jquery';
 
 class FurbabiesList extends Component {
 
-  componentDidMount() {
-    $('.furbabyCard').hover(function(event) {
-      console.log('here ', $(event.currentTarget).find('.furbabyNotes'));
-      $(event.currentTarget).find('.furbabyNotes').toggle();
-    });
-  }
+  // componentDidMount() {
+  //   $('.furbabyCard').mouseenter(function(event) {
+  //     $(event.currentTarget).find('.furbaby').css('transform', 'translateY(-25px)');
+  //   });
+  //   $('.furbabyCard').mouseleave(function(event) {
+  //     $(event.currentTarget).find('.furbaby').css('transform', 'translateY(25px)');
+  //   });
+  // }
 
   render() {
     const { furbabies } = this.props;
+    const furbaby = furbabies[7];
+    console.log('this is  ', furbaby)
     return (
       <div className='mainContainer'>
-      {furbabies.map(furbaby => (
+      {/* {furbabies.map(furbaby => ( */}
         <div key={furbaby.id} className='furbabyCard'>
           <div className='wrapper'>
             <img alt="" src={furbaby.photoUrl} />
-            <div className='furbaby'>
-              <div className='furbabyDetails'>
-                <div className='furbabyName'><b>Name: </b>{furbaby.name}</div>
-                <div className='furbabyBreed'><b>Breed: </b>{furbaby.breed}</div>
-                <div hidden className='furbabyNotes'><b>Notes: </b>Cat is super healthy!</div>
-              </div>
+            <div className='furbabyDetails'>
+              <div className='furbabyName'><b>Name: </b>{furbaby.name}</div>
+              <div className='furbabyBreed'><b>Breed: </b>{furbaby.breed}</div>
+              <div className='furbabyAge'><b>Age: </b>{furbaby.age}</div>
+              <div className='furbabySex'><b>Sex: </b>{furbaby.sex}</div>
+              <div className='furbabySpayed'><b>Spayed: </b>{furbaby.spayed ? 'Yes' : 'No'}</div>
+              <div className='furbabyFivpositive'><b>FIV Positive: </b>{furbaby.fivpositive ? 'Yes' : 'No'}</div>
+              <div className='furbabyComments'><b>Comments: </b>{furbaby.comments}</div>
             </div>
           </div>
         </div>
-        ))}
+        {/* ))} */}
       </div>
     )
   }
@@ -40,15 +46,6 @@ const mapState = state => {
     furbabies: state.furbabies
   }
 }
-
-// const mapDispatch = dispatch => {
-//   return {
-//     submit(info) {
-//       console.log('dispatching')
-//       dispatch(updateFosterThunk(info))
-//     }
-//   }
-// }
 
 const FurbabiesListContainer = connect(mapState, null)(FurbabiesList);
 export default FurbabiesListContainer;
