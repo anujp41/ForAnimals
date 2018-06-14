39235020fca349ff727011c59f4e9595af535a3e
anujp41 <anuj.pant41@gmail.com>
2018-06-13 21:42:52 -0400

    creating seedGenerator and add npm library to get cat image

--- server/db/seedGenerator.js
+++ server/db/seedGenerator.js
@@ -1,36 +1,51 @@
-const Chance = require("chance");
-const chance = new Chance();
+// const Chance = require("chance");
+// const chance = new Chance();
 
-const currentState = ['Adoptable', 'Available as Barn Cat', 'Adoption Pending', 'Return Pending', 'Adopted', 'Fostered', 'Deceased', 'Returned to Colony'];
-const gender = ['Male', 'Female'];
-const goodVars = ['Yes', 'No', 'Unsure'];
-const 
+// const currentState = ['Adoptable', 'Available as Barn Cat', 'Adoption Pending', 'Return Pending', 'Adopted', 'Fostered', 'Deceased', 'Returned to Colony'];
+// const gender = ['Male', 'Female'];
+// const goodVars = ['Yes', 'No', 'Unsure'];
 
-const defaultJSON = {shelterName: 'Parksville',
-  adoptedName: 'Bevo',
-  birthDate: '2015-4-3',
-  intakeDate: '2015-4-27',
-  currentStatus: 'Adopted',
-  size: 'Small',
-  coatColor: 'Calico',
-  coatLength: 'medium',
-  breed: 'Domestic Short hair',
-  gender: 'Female',
-  altered: true,
-  fivStatus: false,
-  felvStatus: false,
-  otherMedical: 'diva problem',
-  behavioralIssues: 'very playful and sweet',
-  goodWithCats: 'Yes',
-  goodWithDogs: 'Unsure',
-  goodWithChildren: 'Yes',
-  specialNeeds: 'Needs to be treated like a diva',
-  bio: 'Bevo was rescued in Parksville',
-  currentLocation: 'Jackson Heights',
-  courtesyListing: false,
-  courtesyListLoc: 'N/A',
-  parentId: 2,
-  youtubeVid: 'https://www.bevodiva.com',
-  photoUrl: 'https://pbs.twimg.com/profile_images/378800000532546226/dbe5f0727b69487016ffd67a6689e75a_400x400.jpeg',
-  microchipNum: 'TVNxxxxxxxxxx',
-  imagesOtherURL: }
+// const capitalize = string => string.replace(/\b\w/g, l => l.toUpperCase());
+
+// const defaultJSON = () => ( {shelterName: chance.name(),
+//   adoptedName: chance.name(),
+//   birthDate: chance.year({min: 2000, max: 2018})+'-'+chance.month({raw: true}).numeric+'-'+chance.integer({ min: 1, max: 28 }),
+//   intakeDate: chance.year({min: 2000, max: 2018})+'-'+chance.month({raw: true}).numeric+'-'+chance.integer({ min: 1, max: 28 }),
+//   currentStatus: currentState[chance.integer({min:0, max: currentState.length-1})],
+//   size: capitalize(chance.word({length: 5})),
+//   coatColor: capitalize(chance.word({length: 7})),
+//   coatLength: capitalize(chance.word({length: 5})),
+//   breed: capitalize(chance.sentence({words: 2})),
+//   gender: gender[chance.integer({min:0, max: gender.length-1})],
+//   altered: chance.bool({likelihood: 95}),
+//   fivStatus: chance.bool({likelihood: 60}),
+//   felvStatus: chance.bool({likelihood: 80}),
+//   otherMedical: capitalize(chance.sentence({words: 5})),
+//   behavioralIssues: capitalize(chance.sentence({words: 7})),
+//   goodWithCats: goodVars[chance.integer({min:0, max: goodVars.length-1})],
+//   goodWithDogs: goodVars[chance.integer({min:0, max: goodVars.length-1})],
+//   goodWithChildren: goodVars[chance.integer({min:0, max: goodVars.length-1})],
+//   specialNeeds: capitalize(chance.sentence({words: 15})),
+//   bio: capitalize(chance.sentence({words: 25})),
+//   currentLocation: chance.address(),
+//   courtesyListing: chance.bool(),
+//   courtesyListLoc: chance.bool({likelihood: 5}),
+//   parentId: 2,
+//   youtubeVid: chance.url({domain: 'www.youtube.com'}),
+//   photoUrl: chance.url({extensions: ['jpg', 'png']}),
+//   microchipNum: 'TVNxxxxxxxxxx',
+//   imagesOtherURL: chance.n(chance.url, chance.integer({ min: 0, max: 20 }), {extensions: ['doc', 'docx', 'pdf']})
+// });
+
+//   const seed = [];
+
+//   for (let i = 0; i < 5; i++) {
+//     seed.push(defaultJSON());
+//   }
+//   console.log(seed)
+
+var cats = require("cats-js");
+ 
+var c = new cats();
+ 
+c.get({type: "jpg", size: "med"}).then(cat => console.log(cat));
