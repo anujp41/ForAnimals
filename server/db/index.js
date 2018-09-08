ed671276e679b905a1f890d1895471291288ab09
anujp41 <anuj.pant41@gmail.com>
2018-09-08 14:20:19 -0400

    updated DB URL

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize('postgres://localhost:5432/forAnimals', {
+const db = new Sequelize(process.env.DATABASE_URL, {
     logging: false
 });
 
