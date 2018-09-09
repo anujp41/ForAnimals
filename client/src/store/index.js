8663c53ad45da4a15a746205ad0c7a795e5cb3a3
anujp41 <anuj.pant41@gmail.com>
2018-09-09 15:22:23 -0400

    further updates to redux store to detect NODE_ENV; middleware not added if production env

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -8,18 +8,20 @@ import filterFurbaby from './filterFurbaby';
 import user from './user';
 import flashMsg from './flashMsg';
 
+let store = null;
+
+if (process.env.NODE_ENVIRONMENT === 'production') {
+  store = combineReducers({ furbabies, parents, furbabyDetail, filterFurbaby, user, flashMsg });
+} else {
+  const reducer = combineReducers({ furbabies, parents, furbabyDetail, filterFurbaby, user, flashMsg });
+  const middleware = process.env.NODE_ENVIRONMENT === 'production' ? applyMiddleware(thunkMiddleware) : applyMiddleware(thunkMiddleware, createLogger({collapsed: true}));
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+  store = createStore(reducer, composeEnhancers(middleware));
+}
+
+export default store;
 export * from './furbabies';
 export * from './parents';
 export * from './furbabyDetail';
 export * from './filterFurbaby';
 export * from './user';
-
-if (process.env.NODE_ENVIRONMENT === 'production') {
-  export default combineReducers({ furbabies, parents, furbabyDetail, filterFurbaby, user, flashMsg });
-  return;
-} else {
-  const reducer = combineReducers({ furbabies, parents, furbabyDetail, filterFurbaby, user, flashMsg });
-  const middleware = process.env.NODE_ENVIRONMENT === 'production' ? applyMiddleware(thunkMiddleware) : applyMiddleware(thunkMiddleware, createLogger({collapsed: true}));
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
-  export default createStore(reducer, composeEnhancers(middleware));
-}
