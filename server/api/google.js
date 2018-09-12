a84c69bf11bc3e32fca3848f268113c211382f94
anujp41 <anuj.pant41@gmail.com>
2018-09-11 21:04:53 -0400

    updated routed for google oauth to show /welcome

--- server/api/google.js
+++ server/api/google.js
@@ -6,7 +6,7 @@ router.get('/', passport.authenticate('google', {scope: 'email'}));
 router.get('/verify',
   passport.authenticate('google', { failureRedirect: `${process.env.PROCESS_URL}` }),
   function (req, res) {
-    res.redirect(`${process.env.PROCESS_URL}`);
+    res.redirect(`${process.env.PROCESS_URL}welcome`);
   }
 )
 
