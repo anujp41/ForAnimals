f25c4029cc4f134b842e6675b5c52c7f88a98236
anujp41 <anuj.pant41@gmail.com>
2018-09-12 20:29:53 -0400

    added reset pw box

--- client/src/components/ResetPW.js
+++ client/src/components/ResetPW.js
@@ -8,9 +8,59 @@ class ResetPW extends Component {
 
   constructor() {
     super();
+    this.renderPWForm = this.renderPWForm.bind(this);
+    this.pwMismatch = this.pwMismatch.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.state = {
-      tokenExpired: null
+      tokenExpired: null,
+      password: '',
+      confirmPassword: '',
+      showMismatch: null
     }
+  }
+
+  handleChange(event) {
+    const {target: {name, value}} = event;
+    this.setState({[name]: value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const {password, confirmPassword} = this.state;
+    if (password !== confirmPassword) {
+      this.setState({showMismatch: true});
+      setTimeout(() => this.setState({showMismatch: false}), 1250);
+      return;
+    }
+  }
+
+  pwMismatch() {
+    const {showMismatch} = this.state;
+    if (showMismatch) return <div className='pw-match'>Password Do Not Match!</div>
+    else return null
+  }
+
+  renderPWForm() {
+    const {password, confirmPassword} = this.state;
+    return (
+      <div className='login-container'>
+        <form onSubmit={this.handleSubmit} autoComplete='off'>
+          <div className='formfield'>
+            <input required className='input' type='password' name='password' value={password} onChange={this.handleChange}/>
+            <label id='no-transform' className='label-text'>Password:</label>
+          </div>
+
+          <div className='formfield'>
+            <input required className='input' type='password' name='confirmPassword' value={confirmPassword} onChange={this.handleChange}/>
+            <label id='no-transform' className='label-text'>Confirm Password:</label>
+            {this.pwMismatch()}
+          </div>
+
+          <button className='button' type='submit'>Reset Password</button>
+        </form>
+      </div>
+    )
   }
 
   componentWillMount() {
@@ -24,7 +74,7 @@ class ResetPW extends Component {
     if (tokenExpired === null) {
       return <div className='loader'></div>
     } else if (tokenExpired === true) {
-      return <div>You can change you PW!</div>
+      return this.renderPWForm();
     } else {
       return <div>Your link has expired. Please try again!</div>
     }
