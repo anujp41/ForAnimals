0409fe64a4dd26b926ae3dfe1c012f861d48f764
anujp41 <anuj.pant41@gmail.com>
2018-09-15 20:54:50 -0400

    refined language and format in access email

--- server/utils/genEmailTemplate.js
+++ server/utils/genEmailTemplate.js
@@ -24,28 +24,27 @@ const genAccessEmailTemplate = (id, email, firstName, lastName) => {
   const accessEmail = {
     body: {
       name: 'For Animals Administrator',
-      intro: [`${firstName} ${lastName} (${email}) is requesting access to the For Animals database website.`, `If you recongnize ${firstName}, please click the GREEN button below to grant access. If you do not recognize him/her, please click the grey button!`],
-      action: [
-        {
-          instructions: 'To grant access, please click here:',
-          button: {
-            color: '#22BC66',
-            text: 'Click to grant access',
-            link: `${process.env.PROCESS_URL}api/auth/userAccess/${id}/?=true`
-          }
-        }, {
-          instructions: `If you do not recognize ${firstName}, click button below:`,
-          button: {
-            color: '#485148',
-            text: 'Click to not grant access',
-            link: `${process.env.PROCESS_URL}api/auth/userAccess/${id}/?=false`
-          }
-        }]
-      },
-      outro: 'Thank you for your prompt action!'
-    };
+      intro: [`${firstName} ${lastName} (${email}) has requested access to the For Animals database website.`, `If you recongnize ${firstName}, please click the GREEN button below to grant access. If you do not recognize him/her, please click the grey button to deny access!`],
+      action: [{
+        instructions: 'To grant access, please click here:',
+        button: {
+          color: '#22BC66',
+          text: 'Click to grant access',
+          link: `${process.env.PROCESS_URL}api/auth/userAccess/${id}?=true`
+        }
+      }, {
+        instructions: `If you do not recognize ${firstName}, click button below:`,
+        button: {
+          color: '#485148',
+          text: 'Click to deny access',
+          link: `${process.env.PROCESS_URL}api/auth/userAccess/${id}?=false`
+        }
+      }]
+    },
+    outro: 'Thank you for your prompt action!'
+  };
   const emailBody = mailGenerator.generate(accessEmail);
   return emailBody;
 };
 
-module.exports = {genPWEmailTemplate, genAccessEmailTemplate};
+module.exports = { genPWEmailTemplate, genAccessEmailTemplate };
