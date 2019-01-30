1fe8e7390dea2f89b4c9e09756de816011214884
anujp41 <anuj.pant41@gmail.com>
2019-01-30 13:37:32 -0500

    updates to client

--- client/src/utils/index.js
+++
@@ -1,7 +0,0 @@
-export { default as compObj } from './compObj';
-export { default as sortFunc } from './sortFunc';
-export { default as handleDate } from './handleDate';
-export { default as checkUser } from './checkUser';
-export { default as checkToken } from './checkToken';
-export { default as resetPW } from './resetPW';
-export { default as provideAccess } from './provideAccess';
